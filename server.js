e06fffc7bf291a4352f4bdef59df899ed75adb64
Yusuf Aji Wibowo <yusuf@skyshi.com>
2017-03-09 06:17:41 +0700

    add port from env (heroku)

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.get('/about', middleware.requireAuthentication, function(req, res){
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT, function(){
+app.listen(process.env.PORT || PORT, function(){
   console.log('Express server started on port '+ PORT +'!');
 });
